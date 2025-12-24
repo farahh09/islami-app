5c819ef2445a00e527282eddc7f5b7f0c8dcbf71
Farah <farahhanyy766@gmail.com>
2025-12-24 20:55:56 +0200

    app color and styles

---
+++ lib/core/app_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppStyles {
+  static TextStyle? titleStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static TextStyle? bodyStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static TextStyle? sebhaStyle = TextStyle(
+      fontSize: 36,
+      color: Colors.white
+  );
+
+  static TextStyle? timeStyle = TextStyle(
+      fontSize: 16,
+      color: Colors.white
+  );
+}
